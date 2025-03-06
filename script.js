568e02fd792c135c9fc3168be2f725c73512745c
GERALDH99 <geraldsepulvedaw@gmail.com>
2025-03-05 21:13:47 -0400

    Se ha progresado en la practica 100%

--- script.js
+++ script.js
@@ -52,7 +52,159 @@ document.querySelector(".search").addEventListener(`click`, () => {
 });
 
 
+function ConPromesas() {
+  console.log("--- Ejemplo API con Promesas (Fetch) ---");
+  fetch("https://jsonplaceholder.typicode.com/photos?_start=0&_limitd2323")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((datos) => {
+      console.log("Datos:", datos);
+      let html;
+      search.addEventListener("click", () => {
+        let idDato = Number(input.value);
+        console.log(typeof idDato);
 
+        let html = "";
+        let datoBuscado = datos.find((dato) => dato.id === idDato);
+
+        if (datoBuscado) {
+          html = `<p>ID: ${datoBuscado.id} </p>
+                    <p>Título: ${datoBuscado.title}</p>
+                    <p>URL: <a href="${datoBuscado.url}" target="_blank">${datoBuscado.url}</a></p>`;
+        } else {
+          html = "<p>Petición Incorrecta</p>";
+        }
+
+
+      });
+
+
+      limpiar.addEventListener("click", () => {
+
+        document.querySelectorAll("p").forEach((e) => {
+          e.innerHTML = "";
+          input.value = '';
+        });
+      });
+    })
+    .catch((error) => {
+      console.error("Error en la petición Fetch:", error);
+    })
+    .finally(() => {
+      console.log("Operación Fetch finalizada\n");
+    });
+}
+
+function llamarApiConCallback(url, callback) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", url);
+  xhr.onload = () => {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      const data = JSON.parse(xhr.responseText);
+      callback(null, data);
+    } else {
+      callback(new Error(`Error de solicitud: ${xhr.status}`), null);
+    }
+  };
+  xhr.onerror = () => {
+    callback(new Error("Error de red"), null);
+  };
+
+  xhr.send();
+}
+
+function ejemploApiCallback() {
+  console.log("--- Ejemplo API con Callback (XMLHttpRequest) ---");
+  llamarApiConCallback(
+    "https://jsonplaceholder.typicode.com/photos?_start=0&_limit=5",
+    (error, datos) => {
+      if (error) {
+        console.error("Error en la llamada:", error);
+      } else {
+        console.log("Datos:", datos);
+
+        search.addEventListener("click", () => {
+          let idDato = Number(input.value);
+          console.log(typeof idDato);
+
+          let html = "";
+          let datoBuscado = datos.find((dato) => dato.id === idDato);
+
+          if (datoBuscado) {
+            html = `<p>ID: ${datoBuscado.id} </p>
+                    <p>Título: ${datoBuscado.title}</p>
+                    <p>URL: <a href="${datoBuscado.url}" target="_blank">${datoBuscado.url}</a></p>`;
+          } else {
+            html = "<p>Petición Incorrecta</p>";
+          }
+
+          
+        });
+
+        limpiar.addEventListener("click", () => {
+          document.getElementById("resultado").innerHTML = "";
+        });
+      }
+    }
+  );
+}
+
+
+
+function vacio(id) {
+  if (id = null) {
+    console.log("Favor de no dejar la funcion vacia:");
+  }
+}
+
+async function ApiAsync() {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/photos?_start=0&_limit=5"
+    );
+    if (!response.ok) {
+      console.log("ERROR AL BUSCAR LOS DATOS");
+    }
+    datos = await response.json();
+    console.log("Datos:", datos);
+    let html;
+    searcher(html, datos);
+  } catch (e) {
+    console.log("Error en la peticion", e);
+  } finally {
+  }
+}
+
+search.addEventListener("click", () => {
+  let idDato = Number(input.value);
+
+
+
+  if (idDato >= 0 && idDato < 5) {
+    let datoBuscado = datos.find((dato) => dato.id == idDato);
+    console.log("Que viene:", datoBuscado);
+    html = `<p>ID: ${datoBuscado.id} </p>
+                      <p>Titulo: ${datoBuscado.title}</p>
+                      <p>URL: ${datoBuscado.url} </p> `;
+    cleaning();
+  } else {
+    html = "<p>Peticion Incorrecta</p>";
+  }
+
+  cleaning();
+  container.insertAdjacentHTML("beforeend", html);
+
+  limpiar.addEventListener("click", cleaning);
+});
+
+function searcher(html, datos) {
+
+}
 
 
 fromEvent(search, "click").subscribe(() => {
@@ -66,11 +218,7 @@ fromEvent(search, "click").subscribe(() => {
     let idDato = Number(input.value);
     let html;
 
-    function vacio() {
-      if (idDato = null) {
-        console.log("Favor de no dejar la funcion vacia:");
-      }
-    }
+
 
     console.log(idDato);
     console.log(input.value);
@@ -80,20 +228,21 @@ fromEvent(search, "click").subscribe(() => {
     }
     else {
       if (idDato >= 0 && idDato < 5) {
-
-        html = `<p>ID: ${datos[idDato - 1].id} </p>
-                      <p>Titulo: ${datos[idDato - 1].title}</p>
-                      <p>URL: ${datos[idDato - 1].url} </p>`;
-
-      }
-      else {
+        let datoBuscado = datos.find((dato) => dato.id == idDato);
+        console.log("Que viene:", datoBuscado);
+        html = `<p>ID: ${datoBuscado.id} </p>
+                          <p>Titulo: ${datoBuscado.title}</p>
+                          <p>URL: ${datoBuscado.url} </p> `;
+        cleaning();
+      } else {
         html = "<p>Peticion Incorrecta</p>";
-
       }
-    }
-    cleaning();
-    container.insertAdjacentHTML("beforeend", html);
 
+      cleaning();
+      container.insertAdjacentHTML("beforeend", html);
+
+      limpiar.addEventListener("click", cleaning);
+    };
   } else if (CallBack.checked) {
     const input = document.querySelector(".input");
     const search = document.querySelector(".search");
@@ -102,114 +251,11 @@ fromEvent(search, "click").subscribe(() => {
     const text = document.getElementsByTagName("p");
     const radioButtons = document.getElementsByName("llamada");
 
-    function llamarApiConCallback(url, callback) {
-      const xhr = new XMLHttpRequest();
-
-      xhr.open("GET", url);
-      xhr.onload = () => {
-        if (xhr.status >= 200 && xhr.status < 300) {
-          const data = JSON.parse(xhr.responseText);
-          callback(null, data);
-        } else {
-          callback(new Error(`Error de solicitud: ${xhr.status}`), null);
-        }
-      };
-      xhr.onerror = () => {
-        callback(new Error("Error de red"), null);
-      };
-
-      xhr.send();
-    }
-
-    function ejemploApiCallback() {
-      console.log("--- Ejemplo API con Callback (XMLHttpRequest) ---");
-      llamarApiConCallback(
-        "https://jsonplaceholder.typicode.com/photos?_start=0&_limit=5",
-        (error, datos) => {
-          if (error) {
-            console.error("Error en la llamada:", error);
-          } else {
-            console.log('Datos:', datos)
-            let html;
-
-            search.addEventListener("click", () => {
-
-              let idDato = Number(input.value);
-              console.log(typeof idDato);
-
-              if (idDato > 0 || idDato <= 5) {
-                html = `<p>ID: ${datos[idDato - 1].id} </p>
-                          <p>Titulo: ${datos[idDato - 1].title}</p>
-                          <p>URL: ${datos[idDato - 1].url} </p> `;
-                cleaning();
-              } else {
-                html = "<p>Peticion Incorrecta</p>";
-
-              }
-
-              container.insertAdjacentHTML("beforeend", html);
-
-            });
-
-            limpiar.addEventListener('click', () => {
-              document.querySelectorAll('p').forEach(e => {
-                e.innerHTML = '';
-              });
-            });
-          }
-        }
-      );
-    }
 
     ejemploApiCallback();
 
 
   } else if (Promesa.checked) {
-    function ConPromesas() {
-      console.log("--- Ejemplo API con Promesas (Fetch) ---");
-      fetch("https://jsonplaceholder.typicode.com/photos?_start=0&_limitd2323")
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-          }
-          return response.json();
-        })
-        .then((datos) => {
-          console.log("Datos:", datos);
-          let html;
-          search.addEventListener("click", () => {
-            let idDato = Number(input.value);
-            console.log(typeof idDato);
-
-            if (idDato >= 0 && idDato < 5) {
-              html = `<p>ID: ${datos[idDato - 1].id} </p>
-                        <p>Titulo: ${datos[idDato - 1].title}</p>
-                        <p>URL: ${datos[idDato - 1].url} </p> `;
-              cleaning();
-            } else {
-              html = '<p>Peticion Incorrecta</p>'
-              cleaning();
-            }
-
-            container.insertAdjacentHTML("beforeend", html);
-
-          });
-
-          limpiar.addEventListener("click", () => {
-
-            document.querySelectorAll("p").forEach((e) => {
-              e.innerHTML = "";
-              input.value = '';
-            });
-          });
-        })
-        .catch((error) => {
-          console.error("Error en la petición Fetch:", error);
-        })
-        .finally(() => {
-          console.log("Operación Fetch finalizada\n");
-        });
-    }
 
     ConPromesas();
 
@@ -217,23 +263,6 @@ fromEvent(search, "click").subscribe(() => {
   } else if (AsyncAwait.checked) {
     //ASYNC AWAIT
 
-    async function ApiAsync() {
-      try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/photos?_start=0&_limit=5"
-        );
-        if (!response.ok) {
-          console.log("ERROR AL BUSCAR LOS DATOS");
-        }
-        const datos = await response.json();
-        console.log("Datos:", datos);
-        let html;
-        searcher(html, datos);
-      } catch (e) {
-        console.log("Error en la peticion", e);
-      } finally {
-      }
-    }
 
     ApiAsync();
 
@@ -244,25 +273,6 @@ fromEvent(search, "click").subscribe(() => {
       });
     }
 
-    function searcher(html, datos) {
-      search.addEventListener("click", () => {
-        let idDato = Number(input.value);
-
-        if (idDato >= 0 && idDato < 5) {
-
-          html = `<p>ID: ${datos[idDato - 1].id} </p>
-                            <p>Titulo: ${datos[idDato - 1].title}</p>
-                            <p>URL: ${datos[idDato - 1].url} </p> `;
-          cleaning();
-        } else {
-          html = "<p>Peticion Incorrecta</p>";
-        }
-        cleaning();
-        container.insertAdjacentHTML("beforeend", html);
-
-        limpiar.addEventListener("click", cleaning);
-      });
-    }
 
 
   }
